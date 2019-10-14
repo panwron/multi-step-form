1527a753187a6f0fb885568e66651c1117d9475b
Maciek Wrona <panwron@gmail.com>
2019-10-14 22:46:23 +0200

    refactor main form component

--- src/components/threeStepForm/ThreeStepForm.js
+++ src/components/threeStepForm/ThreeStepForm.js
@@ -1,42 +1,49 @@
 import React, { Component } from "react";
-import { Formik, Form, Field, ErrorMessage } from "formik";
+import { Formik, Form } from "formik";
 import { string, object, array } from "yup";
 
 import SelectFields from "./SelectFields";
 import CheckBoxFields from "./CheckBoxFields";
 import TextFields from "./TextFields";
 
+const initialValues = {
+  city: "",
+  state: "",
+  colors: [],
+  firstName: "",
+  secondName: "",
+  email: ""
+};
+
+const validationShema = {
+  state: string().required("required"),
+  city: string().required("required"),
+  colors: array().required("required"),
+  firstName: string().required("required"),
+  secondName: string().required("required"),
+  email: string()
+    .email("Invalid email address")
+    .required("required")
+};
+
 class ThreeStepForm extends Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
+
+  handleSubmit = values => {
+    console.table(values);
+    this.setState({ submitedValues: values });
+  };
+
   render() {
     return (
       <div className="form-container">
         <Formik
-          initialValues={{
-            city: "",
-            state: "",
-            colors: [],
-            firstName: "",
-            secondName: "",
-            email: ""
-          }}
-          validationSchema={object().shape({
-            state: string().required("required"),
-            city: string().required("required"),
-            colors: array().required("required"),
-            firstName: string().required("required"),
-            secondName: string().required("required"),
-            email: string()
-              .email("Invalid email address")
-              .required("required")
-          })}
-          onSubmit={values => {
-            console.log(values);
-            this.setState({ submitedValues: values });
-          }}
+          initialValues={initialValues}
+          validationSchema={object().shape(validationShema)}
+          onSubmit={this.handleSubmit}
           render={({ values }) => (
             <Form>
               <SelectFields stateFilter={values.state} />
